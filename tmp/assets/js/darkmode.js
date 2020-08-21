926d487751956c6fee4cd143a35ce710010360e2
helenaemilie <damndanishdude@gmail.com>
2020-08-21 13:08:03 +0200

    darkmode try

---
+++ tmp/assets/js/darkmode.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function switchMode() {
+  var element = document.body;
+  element.classList.toggle("darkmode");
+}
+//# sourceMappingURL=darkmode.js.map
--- tmp/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _thing = _interopRequireDefault(require("./thing.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-(0, _thing["default"])();
-//# sourceMappingURL=index.js.map
